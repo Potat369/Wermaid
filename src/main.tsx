37cdc8e39faf6fd6e95f1934d7b72587bec5b526
Potat369 <yevheniidemian@gmail.com>
2025-02-27 10:30:25 +0200

    Make add game page

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "./pages/Home.tsx";
 import App from "./pages/App.tsx";
+import AddGame from "./pages/games/AddGame.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -11,6 +12,9 @@ createRoot(document.getElementById("root")!).render(
       <Routes>
         <Route element={<App />}>
           <Route index element={<Home />} />
+          <Route path="games">
+            <Route path="add" element={<AddGame />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/games/AddGame.tsx
@@ -0,0 +1,56 @@
+import { FormEvent } from "react";
+
+export default function AddGame() {
+  function submit(ev: FormEvent<HTMLFormElement>) {
+    ev.preventDefault();
+
+    const formData = new FormData(ev.currentTarget);
+    const data = Object.fromEntries(formData);
+
+    fetch(import.meta.env.VITE_SERVER_URL + "api/v1/games", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+  }
+
+  return (
+    <>
+      <form onSubmit={submit}>
+        <label htmlFor="name">Name</label>
+        <br />
+        <input id="name" name="name" type="text" placeholder="Valorant" />
+        <br />
+        <label htmlFor="description">Description</label>
+        <br />
+        <input
+          id="description"
+          name="description"
+          type="text"
+          placeholder="Very cool game"
+        />
+        <br />
+        <label htmlFor="genre">Genre</label>
+        <br />
+        <input id="genre" name="genre" type="text" placeholder="Valorant" />
+        <br />
+        <label htmlFor="release_date">Release Date</label>
+        <br />
+        <input id="release_date" name="releaseDate" type="date" />
+        <br />
+        <label htmlFor="picture_url">Picture Url</label>
+        <br />
+        <input
+          id="picture_url"
+          name="pictureUrl"
+          type="url"
+          placeholder="somelink"
+        />
+        <br />
+        <input type="submit" value="Submi" />
+      </form>
+    </>
+  );
+}
