27865edcc4917745d8b10f28ee04815d80f80c79
Potat369 <yevheniidemian@gmail.com>
2025-03-06 10:44:42 +0200

    Make types for entities from database

---
+++ src/types.ts
@@ -0,0 +1,18 @@
+export interface Game {
+  id: string;
+  slug: string;
+  name: string;
+  description: string;
+  genre: string;
+  releaseDate: Date;
+  pictureUrl: Date;
+  ratings: Array<Rating>;
+}
+
+export interface Rating {
+  id: string;
+  gameId: string;
+  userId: string;
+  rating: number;
+  comment?: string;
+}
