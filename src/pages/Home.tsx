a29075513591cc09b4cbbe3b39347b351ee713f8
Potat369 <yevheniidemian@gmail.com>
2025-03-07 09:16:42 +0200

    Copy main page from initial project

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,22 @@
+import { Link } from "react-router";
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <main className="mx-auto text-center">
+        <h1 className="animate-fade-in mt-36 text-4xl sm:text-5xl md:text-6xl">
+          Wermaid
+        </h1>
+        <h3 className="animate-fade-in animate-delay-600 mt-2 text-zinc-500 opacity-0 sm:text-xl md:text-2xl">
+          Community-driven games review website
+        </h3>
+        <Link
+          to="/discovery"
+          className="animate-fade-in animate-delay-1200 my-48 inline-block rounded-md bg-zinc-100/50 p-2 opacity-0 ring-1 ring-zinc-400 backdrop-blur-sm transition-shadow hover:ring-zinc-500 dark:bg-zinc-900/50 dark:ring-zinc-700"
+        >
+          Get Started
+        </Link>
+      </main>
+    </>
+  );
 }
