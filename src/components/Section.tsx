f7da3a296933a06f78efff4dfc6143679071d2ad
Potat369 <yevheniidemian@gmail.com>
2025-03-07 10:01:30 +0200

    Copy discovery and section from initial project

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import type { Game } from "../types.ts";
+import { Link } from "react-router";
 
 export default function Section({ genre }: { genre: string }) {
   const [games, setGames] = useState<Array<Game>>();
@@ -17,8 +18,32 @@ export default function Section({ genre }: { genre: string }) {
   }, [genre]);
 
   return (
-    <div>
-      {games && games.map((game) => <div key={game.id}>{game.slug}</div>)}
+    <div className="border-y border-zinc-400 bg-zinc-200/25 p-2 dark:border-zinc-700 dark:bg-zinc-800/25">
+      <div className="text-sm sm:text-base md:text-xl">Best in {genre}</div>
+      <div className="flex space-x-2 overflow-x-auto">
+        {games &&
+          games.map((game) => (
+            <Link
+              key={game.id}
+              to={`${game.slug}`}
+              className="group rounded-md bg-zinc-200/50 p-2 inset-ring-1 inset-ring-zinc-400 transition-shadow outline-none hover:inset-ring-zinc-500 focus-visible:inset-ring-zinc-500 dark:bg-zinc-800/50 dark:inset-ring-zinc-700"
+            >
+              <div className="size-24 overflow-hidden rounded-md bg-zinc-400 ring-1 ring-zinc-400 group-hover:ring-zinc-500 group-focus-visible:ring-zinc-500 sm:size-28 md:size-32 dark:bg-zinc-700 dark:ring-zinc-700">
+                {game.pictureUrl && (
+                  <img
+                    src={game.pictureUrl}
+                    alt={game.name}
+                    className="size-full object-cover"
+                  />
+                )}
+              </div>
+              <div className="line-clamp-1 text-xs text-balance text-ellipsis contain-inline-size sm:text-sm md:text-base">
+                {game.name}
+              </div>
+              <div className="text-yellow-500"> ★</div>
+            </Link>
+          ))}
+      </div>
     </div>
   );
 }
--- src/pages/games/Discovery.tsx
+++ src/pages/games/Discovery.tsx
@@ -1,12 +1,15 @@
 import Section from "../../components/Section.tsx";
 
 const genres = ["Tactical+Shoter"];
+
 export default function Discovery() {
   return (
     <>
-      {genres.map((genre) => (
-        <Section key={genre} genre={genre} />
-      ))}
+      <main className="container mx-auto space-y-6 border-zinc-400 bg-zinc-100/50 py-3 sm:border-x dark:border-zinc-700 dark:bg-zinc-900/50">
+        {genres.map((genre) => (
+          <Section key={genre} genre={genre} />
+        ))}
+      </main>
     </>
   );
 }
