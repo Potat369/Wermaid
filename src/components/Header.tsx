de833f22d2c1cd32d536a0dd07abbe513ac89aff
Potat369 <yevheniidemian@gmail.com>
2025-03-28 22:40:07 +0200

    Add nav link to /game/add page in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -45,6 +45,22 @@ export default function Header({ themeData }: { themeData: ThemeData }) {
             </nav>
           </div>
           <div className="flex items-center">
+            {user?.role.includes("ADMIN") && (
+              <NavLink
+                to="/game/add"
+                aria-label="Go to game addition page"
+                className="p-2 text-zinc-500 transition-colors hover:text-current"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  fill="currentColor"
+                  className="size-4"
+                >
+                  <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 344l0-64-64 0c-13.3 0-24-10.7-24-24s10.7-24 24-24l64 0 0-64c0-13.3 10.7-24 24-24s24 10.7 24 24l0 64 64 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-64 0 0 64c0 13.3-10.7 24-24 24s-24-10.7-24-24z" />
+                </svg>
+              </NavLink>
+            )}
             <ThemeSwitcher themeData={themeData} />
             <Link
               to="/profile"
