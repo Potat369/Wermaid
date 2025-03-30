af5cf08a9812e50164d7f7f6895504cd28ee87f9
Potat369 <yevheniidemian@gmail.com>
2025-03-30 10:04:01 +0300

    Add background to discovery page

--- src/pages/games/Discovery.tsx
+++ src/pages/games/Discovery.tsx
@@ -1,11 +1,18 @@
 import Section from "../../components/Section.tsx";
 
-const genres = ["Tactical+Shoter"];
+const genres = ["Shooter", "MOBA", "Strategy", "Horror"];
 
 export default function Discovery() {
   return (
     <>
-      <main className="container mx-auto space-y-6 border-zinc-400 bg-zinc-100/50 py-3 sm:border-x dark:border-zinc-700 dark:bg-zinc-900/50">
+      <main className="relative container mx-auto">
+        <div className="absolute -left-[50%] -z-50 size-96 rounded-full bg-blue-500/10 blur-3xl" />
+        <div className="absolute -top-[50%] -z-50 h-300 w-full rounded-full bg-purple-500/5 blur-3xl" />
+        <div className="absolute -z-50 size-full">
+          <div className="flex justify-center">
+            <div className="-top-[50%] flex h-150 w-[50%] rounded-full bg-pink-500/5 blur-3xl" />
+          </div>
+        </div>
         {genres.map((genre) => (
           <Section key={genre} genre={genre} />
         ))}
