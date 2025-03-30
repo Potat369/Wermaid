ccf1fab7394606acb120ef8c8e4993c8d4a8d21e
Potat369 <yevheniidemian@gmail.com>
2025-03-30 10:09:49 +0300

    Make page highlight on header if you're on it

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router";
+import { NavLink } from "react-router";
 import ThemeSwitcher from "./ThemeSwitcher.tsx";
 import { ThemeData } from "../types.ts";
 import { useContext } from "react";
@@ -14,10 +14,10 @@ export default function Header({ themeData }: { themeData: ThemeData }) {
           <div className="flex items-center">
             <span className="p-2 font-semibold">Wermaid</span>
             <nav className="flex">
-              <Link
+              <NavLink
                 to="/"
                 aria-label="Go to home page"
-                className="p-2 text-zinc-500 transition-colors hover:text-current"
+                className="p-2 text-zinc-500 transition-colors hover:text-current [&.active]:cursor-default [&.active]:text-current/75"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -27,11 +27,11 @@ export default function Header({ themeData }: { themeData: ThemeData }) {
                 >
                   <path d="M575.8 255.5c0 18-15 32.1-32 32.1l-32 0 .7 160.2c0 2.7-.2 5.4-.5 8.1l0 16.2c0 22.1-17.9 40-40 40l-16 0c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1L416 512l-24 0c-22.1 0-40-17.9-40-40l0-24 0-64c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32 14.3-32 32l0 64 0 24c0 22.1-17.9 40-40 40l-24 0-31.9 0c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2l-16 0c-22.1 0-40-17.9-40-40l0-112c0-.9 0-1.9 .1-2.8l0-69.7-32 0c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z" />
                 </svg>
-              </Link>
-              <Link
+              </NavLink>
+              <NavLink
                 to="/discovery"
                 aria-label="Go to discovery page"
-                className="p-2 text-zinc-500 transition-colors hover:text-current"
+                className="p-2 text-zinc-500 transition-colors hover:text-current [&.active]:cursor-default [&.active]:text-current/75"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -41,7 +41,7 @@ export default function Header({ themeData }: { themeData: ThemeData }) {
                 >
                   <path d="M253.3 35.1c6.1-11.8 1.5-26.3-10.2-32.4s-26.3-1.5-32.4 10.2L117.6 192 32 192c-17.7 0-32 14.3-32 32s14.3 32 32 32L83.9 463.5C91 492 116.6 512 146 512L430 512c29.4 0 55-20 62.1-48.5L544 256c17.7 0 32-14.3 32-32s-14.3-32-32-32l-85.6 0L365.3 12.9C359.2 1.2 344.7-3.4 332.9 2.7s-16.3 20.6-10.2 32.4L404.3 192l-232.6 0L253.3 35.1zM192 304l0 96c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-96c0-8.8 7.2-16 16-16s16 7.2 16 16zm96-16c8.8 0 16 7.2 16 16l0 96c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-96c0-8.8 7.2-16 16-16zm128 16l0 96c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-96c0-8.8 7.2-16 16-16s16 7.2 16 16z" />
                 </svg>
-              </Link>
+              </NavLink>
             </nav>
           </div>
           <div className="flex items-center">
@@ -49,7 +49,7 @@ export default function Header({ themeData }: { themeData: ThemeData }) {
               <NavLink
                 to="/game/add"
                 aria-label="Go to game addition page"
-                className="p-2 text-zinc-500 transition-colors hover:text-current"
+                className="p-2 text-zinc-500 transition-colors hover:text-current [&.active]:cursor-default [&.active]:text-current/75"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -62,10 +62,10 @@ export default function Header({ themeData }: { themeData: ThemeData }) {
               </NavLink>
             )}
             <ThemeSwitcher themeData={themeData} />
-            <Link
+            <NavLink
               to="/profile"
               aria-label="Go to profile page"
-              className="p-2 text-zinc-500 transition-colors hover:text-current"
+              className="p-2 text-zinc-500 transition-colors hover:text-current [&.active]:cursor-default [&.active]:text-current/75"
             >
               {user ? (
                 user.displayName
@@ -79,7 +79,7 @@ export default function Header({ themeData }: { themeData: ThemeData }) {
                   <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512l388.6 0c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304l-91.4 0z" />
                 </svg>
               )}
-            </Link>
+            </NavLink>
           </div>
         </div>
       </header>
